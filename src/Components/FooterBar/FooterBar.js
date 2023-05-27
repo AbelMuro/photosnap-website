8831f22bfb8de111783474404720577d33df00bd
abel muro <abelmuro93@gmail.com>
2023-05-26 20:03:38 -0700

    commit

--- src/Components/FooterBar/FooterBar.js
+++ src/Components/FooterBar/FooterBar.js
@@ -2,7 +2,37 @@ import React from 'react';
 import styles from './styles.module.css';
 
 function FooterBar() {
-    return(<footer></footer>)
+    return(
+        <footer className={styles.footer}>
+            <div className={styles.footer_content}>
+                <div className={styles.content_grid}>
+                    <img className={styles.content_grid_logo}/>
+                    <ul className={styles.content_grid_socialIcons}>
+                        <li className={styles.content_grid_socialIcon}></li>
+                        <li className={styles.content_grid_socialIcon}></li>
+                        <li className={styles.content_grid_socialIcon}></li> 
+                        <li className={styles.content_grid_socialIcon}></li>
+                        <li className={styles.content_grid_socialIcon}></li>
+                    </ul>
+                    <ul className={styles.content_grid_navLinks}>
+                        <li className={styles.content_grid_navLink}></li>
+                        <li className={styles.content_grid_navLink}></li>
+                        <li className={styles.content_grid_navLink}></li> 
+                        <li className={styles.content_grid_navLink}></li>
+                        <li className={styles.content_grid_navLink}></li>
+                    </ul>
+                </div>
+                <div className={styles.content_other}>
+                    <button className={styles.inviteButton}>
+                        get an invite
+                    </button>
+                    <p className={styles.copyright}>
+                        Copyright 2019 All Rights Reserved
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default FooterBar;
